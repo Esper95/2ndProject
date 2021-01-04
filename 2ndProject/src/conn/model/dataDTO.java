9776b600e9820bb34f6772c14a25b32b2a8de028
SMT055 <SMT055@DESKTOP-L00AH2P>
2021-01-04 23:55:56 +0900

    수정

--- 2ndProject/src/conn/model/dataDTO.java
+++ 2ndProject/src/conn/model/dataDTO.java
@@ -1,5 +1,53 @@
 package conn.model;
 
 public class dataDTO {
+	private String id;
+	private String pw;
+	private String phone;
+	private String email;
+	
+	
+	
+	
+	public dataDTO(String id, String pw, String phone, String email) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+	public dataDTO(String id, String pw) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	
 }
